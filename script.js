ef1e9262c6d50df46872c705745876e7f6979063
Anastasia <aachuchkalova@gmail.com>
2023-06-28 23:13:24 +0700

    feat: add task output function

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ const tasks=[];
 dom.add.onclick=()=>{
     const newTaskText=dom.new.value;
     if(newTaskText && isNotHaveTask(newTaskText,tasks))
-    addTask(newTaskText);
+    addTask(newTaskText,tasks);
     dom.new.value='';
 }
 //Функция добавления задач
-function addTask(text){
+function addTask(text,list){
     const timeStamp=Date.now();
         const task={
             id:timeStamp,
             text:text,//либо написать просто text
             isComplate:false
         }
-    tasks.push(task);
+    list.push(task);
     console.log(tasks);
 }
 //Проверка существование задачи в массиве задач
@@ -34,4 +34,22 @@ function isNotHaveTask(text,list){
         }
     })
     return isNotHave;
+}
+//Функция вывода списка задач
+function tasksRender(list){
+    let htmlList='';
+    list.forEach((task)=>{
+        const cls= task.isComplate ? "todo__task todo__task-complete" :"todo__task";
+       const taskHtml= `
+       <div id="${task.id}" class="${cls}"            <label class="todo__checkbox">
+                <input type="checkbox" checked="${task.isComplate}">
+                div></div>
+            </label>
+            <div class="todo__task-text">${task.text}</div>
+            <div class="todo__task-del">-</div>
+        </div>
+        `;
+        htmlList+=taskHtml;
+    })
+    dom.tasks.innerHTML=htmlList;
 }
